614c75494644f29cb8e3b65aad8c6a60edb80a39
Sandro Covo <sandro@covo.ch>
2013-10-25 22:20:07 +0200

    Add existing files

---
+++ views/generic.py
@@ -0,0 +1,41 @@
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView,\
+                                 DeleteView, DetailView, ArchiveIndexView
+
+app_specific = {}
+
+class ExtraContext(object):
+    extra = {}
+
+    def get_context_data(self, **kwargs):
+        context = super(ExtraContext, self).get_context_data(**kwargs)
+        for key, value in self.extra.iteritems():
+            context[key] = value(context) if callable(value) else value
+        for key, value in app_specific[self.extra['appname']].iteritems():
+            context[key] = value(context) if callable(value) else value
+        return context
+
+class ExtraArchiveIndexView(ExtraContext,ArchiveIndexView):
+    pass
+
+
+class ExtraDetailView(ExtraContext, DetailView):
+    pass
+
+
+class ExtraTemplateView(ExtraContext, TemplateView):
+    pass
+
+
+class ExtraListView(ExtraContext, ListView):
+    pass
+
+
+class ExtraCreateView(ExtraContext, CreateView):
+    pass
+
+
+class ExtraUpdateView(ExtraContext, UpdateView):
+    pass
+
+class ExtraDeleteView(ExtraContext, DeleteView):
+    pass
