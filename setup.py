a3f0b7a66c3385d11dbcfd4fe0ebb468a2b0ac72
Sandro Covo <sandro@covo.ch>
2013-10-26 15:18:36 +0200

    Move all files into seperate folder

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name='django-utils',
+    version='1.0',
+    author='Sandro Covo',
+    author_email='sandro@covo.ch',
+    packages=['djangoutils'],
+    description='django-utils is a python library to simplify the use of django.',
+    install_requires=[
+        'django',
+    ],
+)
