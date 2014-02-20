9847c412a6a3ef2acb7883214ba47e7cd22ebd61
Sandro Covo <sandro@covo.ch>
2014-02-20 18:01:17 +0100

    Minor version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='django-utils',
-    version='1.0',
+    version='1.1',
     author='Sandro Covo',
     author_email='sandro@covo.ch',
     packages=['djangoutils', 'djangoutils.views'],
