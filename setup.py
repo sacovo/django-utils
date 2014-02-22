1a468cad0e36003cf986d9b358ca058e3fbaa6aa
Sandro Covo <sandro@covo.ch>
2014-02-22 20:50:46 +0100

    Add templatetags package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 setup(
     name='django-utils',
-    version='1.2',
+    version='1.2.1',
     author='Sandro Covo',
     author_email='sandro@covo.ch',
-    packages=['djangoutils', 'djangoutils.views'],
+    packages=['djangoutils', 'djangoutils.views', 'djangoutils.templatetags'],
     description='django-utils is a python library to simplify the use of django.',
     install_requires=[
         'django',
